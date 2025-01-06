5c2f3e7db3ebddb64d4a370a68e19f03f87a3f0b
Deepnasree <deepnabachu@gmail.com>
2025-01-06 12:25:34 +0530

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,44 @@
+export const data = [
+    {
+        Question: 'What is Redux?',
+        Option1: 'Redux is State Management System',
+        Option2: 'Redux is State',
+        Option3: 'Redux is Data Type',
+        Option4: 'Redux is a part of HTML'
+    },
+    {
+        Question: 'Which principle is NOT part of Redux?',
+        Option1: 'Single source of truth',
+        Option2: 'State is read-only',
+        Option3: 'Changes are made with pure functions',
+        Option4: 'Multiple stores for multiple components'
+    },
+    {
+        Question: 'What is an action in Redux?',
+        Option1: 'An object that describes what to do',
+        Option2: 'A function that fetches data',
+        Option3: 'A middleware tool',
+        Option4: 'A reducer that changes state'
+    },
+    {
+        Question: 'What is a reducer in Redux?',
+        Option1: 'A pure function that returns the next state',
+        Option2: 'A function to dispatch actions',
+        Option3: 'A method to fetch data',
+        Option4: 'A tool to update the DOM'
+    },
+    {
+        Question: 'Which middleware is commonly used with Redux?',
+        Option1: 'Redux Thunk',
+        Option2: 'Axios',
+        Option3: 'Express',
+        Option4: 'Lodash'
+    },
+    {
+        Question: 'What is the purpose of the Redux store?',
+        Option1: 'To hold the entire state of the application',
+        Option2: 'To manage user authentication',
+        Option3: 'To fetch data from APIs',
+        Option4: 'To handle routing in the application'
+    }
+];
